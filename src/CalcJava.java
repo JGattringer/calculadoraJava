146d8ceb2e45d677822a7dc8f1df22b457a45f46
JGattringer <joaogattringer@hotmail.com>
2024-06-03 20:24:23 -0300

    primeiro commit

---
+++ src/CalcJava.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CalcJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero...");
+        int primeiroNumero = sc.nextInt();
+        System.out.println("Digite o segundo numero...");
+        int segundoNumero = sc.nextInt();
+
+        System.out.println("""
+                Agora escolha a operacao que deseja realizar:
+                1 - adicao
+                2 - subtracao
+                3 - multiplicacao
+                4 - divisao
+                """);
+        int opcao = sc.nextInt();
+
+        switch (opcao) {
+            case 1:
+                // code block
+                break;
+            case 2:
+                // code block
+                break;
+            case 3:
+                // code block
+                break;
+            case 4:
+                // code block
+                break;
+            default:
+                // code block
+        }
+
+
+        sc.close();
+    }
+}
